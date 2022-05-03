82cbd021444c0b2371424f7c59615cf3261b58bc
Geovane Santos <geovane_n.s@outlook.com>
2022-05-03 17:57:33 -0300

    Primeira Rota

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
+import routes from "./routes.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 const port = 3334;
-app.listen(port, console.log(`Server port ${port}`));
+
+app.listen(port, console.log(`Server port ${ port }`));
